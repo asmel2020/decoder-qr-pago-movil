70c98ea939822096f76d44f38c90a097b8fe3a52
Danny Gonzalez <jesusgalicia03@gmail.com>
2026-07-22 19:44:39 -0300

    fix: write .npmrc to ~/.npmrc instead of local

--- scripts/release.sh
+++ scripts/release.sh
@@ -13,8 +13,22 @@ fi
 cd "$PKG_DIR"
 
 BUMP=${1:-patch}
-VERSION=$(npm version "$BUMP" --no-git-tag-version)
-echo "Bumped to $VERSION"
+
+VERSION=$(node -e "
+const pkg = require('./package.json');
+const [major, minor, patch] = pkg.version.split('.').map(Number);
+let v;
+switch ('$BUMP') {
+  case 'major': v = \`\${major+1}.0.0\`; break;
+  case 'minor': v = \`\${major}.\${minor+1}.0\`; break;
+  default:     v = \`\${major}.\${minor}.\${patch+1}\`;
+}
+pkg.version = v;
+require('fs').writeFileSync('./package.json', JSON.stringify(pkg, null, 2) + '\n');
+console.log('v' + v);
+")
+
+echo "Bumped to v$VERSION"
 
 cd - > /dev/null
 
